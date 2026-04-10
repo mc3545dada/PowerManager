808797cd554082f37301e7f664baca1186df72bd
mc3545dada <1811410374@qq.com>
2026-04-10 17:11:33 +0800

    feat:改为默认打开0.02的功率补偿，以稳定为主

--- power_manager.h
+++ power_manager.h
@@ -11,8 +11,8 @@
 #include <vector>
 
 #define M_Too_Small_AllErrors 500.0
-// #define M_Enable_PowerCompensation
-#define M_Power_Compensation_Alpha 0.05
+#define M_Enable_PowerCompensation
+#define M_Power_Compensation_Alpha 0.02
 #define M_Motor_ReservedPower_Border 54.0
 #define M_PerMotor_ReservedPower 8.0
 
